03c71cb06912edc6aad65f1cf135e9ee0a569921
Shriya <shriya0690@gmail.com>
2023-01-15 19:46:57 +0530

    Deployment changes

--- backend/server.js
+++ backend/server.js
@@ -8,18 +8,6 @@ const messageRoutes = require("./routes/messageRoutes");
 const app = express();
 const path = require('path');
 dotenv.config();
-var cors = require('cors')
-
-app.use(cors());
-router.get("/", (req, res) => {
-    res.setHeader("Access-Control-Allow-Origin", "*")
-    res.setHeader("Access-Control-Allow-Credentials", "true");
-    res.setHeader("Access-Control-Max-Age", "1800");
-    res.setHeader("Access-Control-Allow-Headers", "content-type");
-    res.setHeader("Access-Control-Allow-Methods", "PUT, POST, GET, DELETE, PATCH, OPTIONS");
-})
-
-
 connectDB();
 // To accept JSON data.
 app.use(express.json()); // to accept JSON files.
