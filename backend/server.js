a14ba2959e726de9ce31ac2b2fbcc2f8d4684eac
Shriya <shriya0690@gmail.com>
2023-01-15 21:44:57 +0530

    URL

--- backend/server.js
+++ backend/server.js
@@ -10,7 +10,7 @@ const path = require('path');
 const cors = require('cors');
 
 const corsOptions = {
-    origin: 'https://chitchat-shriyasankhyan.netlify.app',
+    origin: process.env.ORIGIN,
     credentials: true,            //access-control-allow-credentials:true
     optionSuccessStatus: 200
 }
